a94bd66faafd624288d0628b1eeb70a3dec0bec3
Kartik Padmanabhan <kartik.apache@gmail.com>
2014-04-26 19:06:35 -0700

    third commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,36 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-	        inv <- NULL
-        set <- function(y) {
-			                x <<- y
-		                inv <<- NULL
-						        }
-		        get <- function() x
-		        setinverse <- function(inverse) inv <<- inverse
-				        getinverse <- function() inv
-				        list(set = set, get = get,
-							              setinverse = setinverse,
-										               getinverse = getinverse)
-						        
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-	        m <- x$getinverse()
-        if(!is.null(m)) {
-			                message("getting cached matrix")
-		                return(m)
-						        }
-		        data <- x$get()
-		        if(det(data)<-0){
-					                message("matrix is singular, and cannot be inversed!")
-				        }
-				        
-				        m <- solve(data, ...)
-				        x$setinverse(m)
-						        m
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  data <- x$get()
+  if(det(data)<-0){
+    message("matrix is singular, and cannot be inversed!")
+  }
+  
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
-
