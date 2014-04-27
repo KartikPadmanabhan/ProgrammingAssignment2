e5c852d3f7f7459a64370aee8186f106c7363989
Kartik Padmanabhan <kartik.apache@gmail.com>
2014-04-27 12:49:13 -0700

    final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,21 @@
+#
+#MATRIX INVERSION PROGRAMMING ASSIGNMENT:
+#----------------------------------------  
+#> source('~/Desktop/github_repo/ProgrammingAssignment2/cachematrix.R') 
+#> x <- makeCacheMatrix(matrix(c(1, 4, 9, 3, 7, 2, 8, 4, 1), ncol=3))  
+#> x$get()
+#[,1] [,2] [,3]
+#[1,]    1    3    8
+#[2,]    4    7    4
+#[3,]    9    2    1
+#> inverse = cacheSolve(x)
+#> inverse
+#[,1]        [,2]        [,3]
+#[1,]  0.002898551 -0.03768116  0.12753623
+#[2,] -0.092753623  0.20579710 -0.08115942
+#[3,]  0.159420290 -0.07246377  0.01449275
+
+
 #makeCacheMatrix creates a special "matrix", a list containing function to set/get values of matrix
 #and set/get the value of inverse of matrix
 
