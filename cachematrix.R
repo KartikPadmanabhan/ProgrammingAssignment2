2c803dd460e065598f4c83ac696b3e55d33605a4
Kartik Padmanabhan <kartik.apache@gmail.com>
2014-04-26 19:33:40 -0700

    final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#makeCacheMatrix creates a special "matrix", a list containing function to set/get values of matrix
+#and set/get the value of inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
+  
+  #Setter function for setting the value of matrix
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
+  
+  #Setter function for getting the value of matrix
   get <- function() x
+  
+  #Setter function for setting the value of matrix
   setinverse <- function(inverse) inv <<- inverse
+  
+  #Getter function for getting the value of matrix
   getinverse <- function() inv
+  
+  
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -19,20 +27,36 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special matrix that is returned by makeCacheMatrix above.
+# if the inverse already exists or has been already calculated, it simply retrieves the inverse from
+#cache
 
 cacheSolve <- function(x, ...) {
+  #try to get inverse from cache first
   m <- x$getinverse()
+  #if inverse of the matrix exists (that is if computed m above isn't null)
   if(!is.null(m)) {
     message("getting cached matrix")
+    #simply return this inverse
     return(m)
   }
+  
+  #if inverse doesn't exists get the actual matrix to compute inverse
   data <- x$get()
+  
+  #check for singularity...that is if matrix cannot be inversed determitent is 0
+  #NOTE that this step isn't really needed here as we are assuming that that matrix 
+  #PROVIDED TO US IS ALWAYS INVERTIBLE. 
+  #But this condition is a good check to ensure we never go wrong
   if(det(data)<-0){
     message("matrix is singular, and cannot be inversed!")
   }
   
+  #calculates the inverse using solve()
   m <- solve(data, ...)
+  
+  #set the inverse back into the cache for the computed inverse above.
   x$setinverse(m)
+  
   m
 }
